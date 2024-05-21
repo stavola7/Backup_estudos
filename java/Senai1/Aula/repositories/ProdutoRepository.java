cc33fc6db18343061eb2193ec3c9ba5709021863
alexandre-stavola <115109870+stavola7@users.noreply.github.com>
2024-05-20 21:25:44 -0300

    Delete java/Senai1/Aula directory

--- java/Senai1/Aula/repositories/ProdutoRepository.java
+++
@@ -1,7 +0,0 @@
-package Senai1.Aula.repositories;
-
-import Senai1.Aula.entidades.Produto;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
-}
--- java/Senai1/Aula/repositories/TurmaRepository.java
+++
@@ -1,7 +0,0 @@
-package Senai1.Aula.repositories;
-
-import Senai1.Aula.entidades.Turma;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TurmaRepository extends JpaRepository<Turma, Integer> {
-}
